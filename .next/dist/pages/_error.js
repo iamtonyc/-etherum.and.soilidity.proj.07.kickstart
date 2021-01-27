130c8205fc40ea6bee5989077dcb84c6636ff9dd
Tony <iamtonycheung@gmail.com>
2021-01-27 18:08:19 +0800

    upto lesson 151

---
+++ .next/dist/pages/_error.js
@@ -0,0 +1,4 @@
+'use strict';
+
+module.exports = require('next\\dist\\lib\\error.js');
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm5vZGVfbW9kdWxlc1xcbmV4dFxcZGlzdFxccGFnZXNcXF9lcnJvci5qcyJdLCJuYW1lcyI6WyJtb2R1bGUiLCJleHBvcnRzIiwicmVxdWlyZSJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxPQUFPLEFBQVAsVUFBaUIsQUFBakIiLCJmaWxlIjoiX2Vycm9yLmpzP2VudHJ5Iiwic291cmNlUm9vdCI6IkY6L0NfZHJpdmVfdG9ueS9kZXYvT3RoZXIuV29ya3NwYWNlL2V0aGVyZXJ1bS5hbmQuc29saWRpdHkuZGV2ZWxvcGVyL3Byb2ouMDcva2lja3N0YXJ0In0=
---
+++ .next/dist/pages/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'F:\\C_drive_tony\\dev\\Other.Workspace\\ethererum.and.solidity.developer\\proj.07\\kickstart\\pages\\index.js?entry';
+
+exports.default = function () {
+    return _react2.default.createElement('h1', {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 4
+        }
+    }, 'This is the campaign list page !!!!');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxpbmRleC5qcyJdLCJuYW1lcyI6WyJSZWFjdCJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTyxBQUVQOzs7Ozs7OztrQkFBZSxZQUFLLEFBQ2hCOzJCQUFPLGNBQUE7O3NCQUFBO3dCQUFBO0FBQUE7QUFBQSxLQUFBLEVBQVAsQUFBTyxBQUNWO0FBRkQiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiRjovQ19kcml2ZV90b255L2Rldi9PdGhlci5Xb3Jrc3BhY2UvZXRoZXJlcnVtLmFuZC5zb2xpZGl0eS5kZXZlbG9wZXIvcHJvai4wNy9raWNrc3RhcnQifQ==
